caf10f1fd52a9693a2870cc9a4601ab8e4a33ba1
Julien Cros <julien.cros86@gmail.com>
2024-01-06 11:57:29 +0100

    feat: loading mobile friendly

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,15 +6,15 @@ import { motion } from "framer-motion";
 export default function Loading() {
   return (
     <div className="fixed w-full h-full">
-      <div className="w-full h-full flex flex-col gap-4 justify-center items-center">
-        <h3 className="text-8xl text-center font-black whitespace-pre">
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <h3 className="text-4xl lg:text-8xl text-center font-black pb-4 whitespace-pre">
           The espresso
           <br /> Universe
         </h3>
-        <div className="w-1/6 h-4 justify-center items-center rounded-full border-2 border-black">
+        <div className="w-1/4 md:w-1/6 h-3 md:h-4 justify-center items-center rounded-full border-2 border-black">
           <motion.div
             onAnimationStart={() => console.log("start")}
-            className="h-[12px] bg-black w-1/6 rounded-full"
+            className="h-[8px] md:h-[12px] bg-black w-1/4 md:w-1/6 rounded-full"
             key="loading"
             initial={{ opacity: 1, width: "0%" }}
             animate={{ opacity: 1, width: "100%" }}
@@ -24,6 +24,7 @@ export default function Loading() {
             }}
           ></motion.div>
         </div>
+				<p>0%</p>
       </div>
     </div>
   );
