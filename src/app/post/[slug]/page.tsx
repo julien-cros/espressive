18dfd7b10ebdabe0926ea0ea4a05b9e97a72c64b
Julien Cros <julien.cros86@gmail.com>
2024-01-07 11:31:12 +0100

    fix: rm title in mdx

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -3,6 +3,7 @@
 import { allPosts } from "contentlayer/generated";
 import Render from "@/components/Render";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 type PostPageProps = {
   params?: {
@@ -16,8 +17,11 @@ export default async function PostPage({ params }: PostPageProps) {
   return (
     <div className="w-full h-full flex flex-col">
       <div className="h-full w-full pt-16 flex justify-center text-2xl md:text-5xl font-bold">
-        <h2>espressive</h2>
+				<h2>espressive</h2>
       </div>
+			<div className="h-full w-full pt-16 flex justify-center text-3xl md:text-5xl font-bold">
+				<h3>{post?.title}</h3>
+			</div>
       <div className="pt-10 md:pt-20">
         <Render code={post?.body.code} />
       </div>
