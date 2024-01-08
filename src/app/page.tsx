f277e0690a3e2d5106b8905c413ef88f7bd8cdc1
Julien Cros <julien.cros86@gmail.com>
2024-01-08 10:45:45 +0100

    fix: animation faster

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
                                 initial={{ opacity: 0, y: "5%" }}
                                 animate={{ opacity: 1, y: "0%" }}
                                 transition={{
-                                  duration: 1.5,
+                                  duration: 1,
                                 }}
 																exit={{ opacity: 0, y: "5%" }}
                               >
