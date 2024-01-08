07bd2e64ebfbf1012f82eeaee71402da478aabad
Julien Cros <julien.cros86@gmail.com>
2024-01-08 18:37:50 +0100

    fix: more padding for mobile

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -13,8 +13,8 @@ export default function Render({ code }: RenderProps) {
   const MDX = useMDXComponent(code);
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
-      <article className="prose lg:prose-lg px-5 z-10">
+    <div className="w-full h-full flex flex-col justify-center items-center px-10 ">
+      <article className="prose lg:prose-lg z-10 ">
         <MDX />
       </article>
     </div>
