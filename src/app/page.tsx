97daef96b8bda1f37d9babbeacf2f0b3910070a3
Julien Cros <julien.cros86@gmail.com>
2024-01-10 19:45:10 +0100

    feat: add some animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,92 +24,103 @@ export default function Home() {
       {loading ? (
         <Loader />
       ) : (
-        <main className="flex h-full w-full flex-col items-center pb-4 md:pb-10 mt-16">
-          <div className="h-full w-full flex justify-center text-2xl md:text-5xl font-bold">
-            <h2>espressive</h2>
-          </div>
-          <div className="w-3/4 lg:w-2/3 h-full mt-10">
-            <div className="border-t-4 md:border-t-8 border-black w-full h-full">
-              <div className="w-full h-10 md:h-20 lg:h-[120px] flex items-center ">
-                <p className="text-4xl md:text-7xl lg:text-9xl font-black lg:tracking-[-0.4rem]">
-                  The
-                </p>
-              </div>
-              <div className="w-full h-10 md:h-20  lg:h-[120px] border-t-4 md:border-t-8 border-black  flex items-center justify-end">
-                <p className="text-right text-4xl md:text-7xl lg:text-9xl font-black lg:tracking-[-0.4rem] pr-2">
-                  Espresso
-                </p>
-              </div>
-              <div className="w-full h-10 md:h-20 pt-5  lg:h-[120px] flex items-center border-t-4 md:border-t-8 border-black ">
-                <p className="text-4xl md:text-7xl lg:text-9xl font-black lg:tracking-[-0.4rem]">
-                  Universe
-                </p>
-              </div>
-              <motion.div
-                key="posts-bottom-animation"
-                initial={{ opacity: 0, y: "5%" }}
-                animate={{ opacity: 1, y: "0%" }}
-                transition={{
-                  duration: 1,
-                }}
-              >
-                <div className="gap-4 pt-10 md:pt-36">
-                  {allPosts
-                    .sort((a, b) => compareDesc(a.createdAt, b.createdAt))
-                    .map((p) => (
-                      <div
-                        key={p.title}
-                        className="pt-4 md:p-10 h-36 overflow-y-hidden"
-                      >
+        <motion.div
+          key="posts-bottom-animation"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 0.5,
+          }}
+        >
+          <main className="flex h-full w-full flex-col items-center pb-4 md:pb-10 mt-16 ">
+            <div className="h-full w-full flex justify-center text-2xl md:text-5xl font-bold">
+              <h2>espressive</h2>
+            </div>
+            <div className="w-3/4 lg:w-2/3 h-full mt-10">
+              <div className="border-t-4 md:border-t-8 border-black w-full h-full">
+                <div className="w-full h-10 md:h-20 lg:h-[120px] flex items-center ">
+                  <p className="text-4xl md:text-7xl lg:text-9xl font-black lg:tracking-[-0.4rem]">
+                    The
+                  </p>
+                </div>
+                <div className="w-full h-10 md:h-20  lg:h-[120px] border-t-4 md:border-t-8 border-black  flex items-center justify-end">
+                  <p className="text-right text-4xl md:text-7xl lg:text-9xl font-black lg:tracking-[-0.4rem] pr-2">
+                    Espresso
+                  </p>
+                </div>
+                <div className="w-full h-10 md:h-20 pt-5  lg:h-[120px] flex items-center border-t-4 md:border-t-8 border-black ">
+                  <p className="text-4xl md:text-7xl lg:text-9xl font-black lg:tracking-[-0.4rem]">
+                    Universe
+                  </p>
+                </div>
+                <motion.div
+                  key="posts-bottom-animation"
+                  initial={{ opacity: 0, y: "5%" }}
+                  animate={{ opacity: 1, y: "0%" }}
+                  transition={{
+                    duration: 1,
+                  }}
+                >
+                  <div className="gap-4 pt-10 md:pt-36">
+                    {allPosts
+                      .sort((a, b) => compareDesc(a.createdAt, b.createdAt))
+                      .map((p) => (
                         <div
-                          className="pl-2 md:pl-10 border-t-2 border-black"
                           key={p.title}
+                          className="pt-4 md:p-10 h-36 overflow-y-hidden"
                         >
-                          <div className="grid grid-flow-row-dense grid-cols-3 lg:grid-cols-2">
-                            <Link
-                              href={p.url}
-                              className="col-span-2 lg:col-span-1"
-                              onMouseOver={() => {
-                                setDisplay(true);
-                                setPostName(p.title);
-                              }}
-                              onMouseLeave={() => {
-                                setDisplay(false);
-                                setPostName("");
-                              }}
-                            >
-                              <h2 className="text-md md:text-2xl">{p.title}</h2>
-                              <h3 className=" text-slate-600 text-sm md:text-xl">
-                                {p.updatedAt}
-                              </h3>
-                            </Link>
-                            <AnimatePresence>
-                              {display && postName === p.title && (
-                                <motion.div
-                                  className={`pl-4 md:pl-10 pr-4 ${
-                                    display ? "hidden md:block" : "hidden"
-                                  } `}
-                                  key="posts-bottom-animation"
-                                  initial={{ opacity: 0, y: "5%" }}
-                                  animate={{ opacity: 1, y: "0%" }}
-                                  transition={{
-                                    duration: 1,
-                                  }}
-                                  exit={{ opacity: 0, y: "5%" }}
-                                >
-                                  <Render code={p.body.code} />
-                                </motion.div>
-                              )}
-                            </AnimatePresence>
+                          <div
+                            className="pl-2 md:pl-10 border-t-2 border-black"
+                            key={p.title}
+                          >
+                            <div className="grid grid-flow-row-dense grid-cols-3 lg:grid-cols-2">
+                              <Link
+                                href={p.url}
+                                className="col-span-2 lg:col-span-1"
+                                onMouseOver={() => {
+                                  setDisplay(true);
+                                  setPostName(p.title);
+                                }}
+                                onMouseLeave={() => {
+                                  setDisplay(false);
+                                  setPostName("");
+                                }}
+                              >
+                                <h2 className="text-md md:text-2xl">
+                                  {p.title}
+                                </h2>
+                                <h3 className=" text-slate-600 text-sm md:text-xl">
+                                  {p.updatedAt}
+                                </h3>
+                              </Link>
+                              <AnimatePresence>
+                                {display && postName === p.title && (
+                                  <motion.div
+                                    className={`pl-4 md:pl-10 pr-4 ${
+                                      display ? "hidden md:block" : "hidden"
+                                    } `}
+                                    key="posts-bottom-animation"
+                                    initial={{ opacity: 0, y: "5%" }}
+                                    animate={{ opacity: 1, y: "0%" }}
+                                    transition={{
+                                      duration: 1,
+                                    }}
+                                    exit={{ opacity: 0, y: "5%" }}
+                                  >
+                                    <Render code={p.body.code} />
+                                  </motion.div>
+                                )}
+                              </AnimatePresence>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    ))}
-                </div>
-              </motion.div>
+                      ))}
+                  </div>
+                </motion.div>
+              </div>
             </div>
-          </div>
-        </main>
+          </main>
+        </motion.div>
       )}
     </>
   );
--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -2,6 +2,7 @@
 
 import { useMDXComponent } from "next-contentlayer/hooks";
 import React from "react";
+import { motion }	from "framer-motion";
 
 type RenderProps = {
   code: string | undefined;
@@ -14,9 +15,16 @@ export default function Render({ code }: RenderProps) {
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center px-10 ">
+			<motion.div
+				key="posts-bottom-animation"
+				initial={{ opacity: 0, y: "2%" }}
+				animate={{ opacity: 1, y: "0%" }}
+				transition={{ duration: 0.5 }}
+			>
       <article className="prose lg:prose-lg z-10 ">
         <MDX />
       </article>
+			</motion.div>
     </div>
   );
 }
